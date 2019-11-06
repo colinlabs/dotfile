c6e6708d4a2457e4d15e4f96d2a9bbc8f4ab8448
Colin.Lee <geek.colin@gmail.com>
2019-11-06 17:12:42 +0800

    update

--- mac_setup.sh
+++ mac_setup.sh
@@ -6,5 +6,5 @@ defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
 defaults -currentHost write -globalDomain AppleFontSmoothing -int 3
 
 # password lenght
- pwpolicy -clearaccountpolicies
+pwpolicy -clearaccountpolicies
 
